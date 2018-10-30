c3e235862ea0298cc9c75548c6e3bccbe81cba30
Jojo E. Tutor <jehrave003@gmail.com>
2018-10-30 18:46:08 +0800

    key list

--- src/Calculator/Keypad/Keypad.js
+++ src/Calculator/Keypad/Keypad.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import KeysList from './KeysList'
+import { Hotkey, Hotkeys, HotkeysTarget } from "@blueprintjs/core";
+import KeysList, { Special, Exclude } from './KeysList'
 import Key from './Key/Key'
 import './Keypad.css'
 
@@ -7,7 +8,24 @@ class Keypad extends Component {
   renderKeys(list) {
     return list.map(e => <Key key={e} keypadKey={e} />)
   }
+  renderHotkeys() {
+    return (
+      <Hotkeys>
+          {KeysList
+            .filter(e => !Exclude.includes(e))
+            .map(e => (
+              <Hotkey
+                key={e}
+                global={true}
+                combo={Object.keys(Special).includes(e) ? Special[e] : e}
+                onKeyDown={() => console.log("Awesome!", e)}
+              />
+          ))}
+      </Hotkeys>
+    )
+  }
   render() {
+    
     return (
       <div className="Keypad">
         { this.renderKeys(KeysList) }
@@ -16,4 +34,4 @@ class Keypad extends Component {
   }
 }
 
-export default Keypad
+export default HotkeysTarget(Keypad)
--- src/Calculator/Keypad/KeysList.js
+++ src/Calculator/Keypad/KeysList.js
@@ -1,6 +1,18 @@
 export default [
-    '7', '8', '9', 'del', 'ac',
+    '7', '8', '9', 'clear', 'bksp',
     '4', '5', '6', 'x', '÷',
     '1', '2', '3', '+', '-',
     '0', '.', '%', 'ans', '='
+]
+
+export const Special = {
+    'bksp': 'backspace',
+    'clear': 'space',
+    '+': 'plus',
+    '-': 'minus',
+    '%': 'shift + 5'
+}
+
+export const Exclude = [
+    'ans', '÷'
 ]
